381161595b2c1ec999b9b3ee125798fa24da8df8
praveenkumar09 <tpkprav@gmail.com>
2023-11-06 07:43:40 +0800

    Added mockito and modified to use mockito

--- timezones/src/test/java/com/udacity/timezones/service/TimeZoneServiceTest.java
+++ timezones/src/test/java/com/udacity/timezones/service/TimeZoneServiceTest.java
@@ -1,13 +1,34 @@
 package com.udacity.timezones.service;
 
 import com.udacity.timezones.client.FakeWorldTimeApiHttpClient;
+import com.udacity.timezones.client.WorldTimeApiHttpClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+
+@ExtendWith(MockitoExtension.class)
 class TimeZoneServiceTest {
+
+    private TimeZoneService timeZoneService;
+
+    @Mock
+    WorldTimeApiHttpClient worldTimeApiHttpClient;
+
+    @BeforeEach
+    void init(){
+        timeZoneService = new TimeZoneService(worldTimeApiHttpClient);
+    }
 
     @Test
     void getAvailableTimezoneText() {
@@ -20,4 +41,13 @@ class TimeZoneServiceTest {
         System.out.println("Expected Result ===>"+expectedResult);
         assertTrue(result.contains(expectedResult));
     }
+
+    @Test
+    void getAvailbleTimeZoneTest_withMockito(){
+        List<String> validTimeZones = List.of("Bangalore","Chennai","Delhi","Hyderabad","Mumbai");
+        when(worldTimeApiHttpClient.getValidTimeZones(anyString())).thenReturn(validTimeZones);
+        String result = timeZoneService.getAvailableTimezoneText("India");
+        assertTrue(result.contains(String.join(", ",validTimeZones)));
+    }
+
 }
